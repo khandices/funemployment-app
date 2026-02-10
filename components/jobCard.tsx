4f5bb6ddfcfab06b6467c98f71c49c0779581fca
khandices <s.khandice@gmail.com>
2026-02-10 14:54:06 -0500

    Update styling for job list

--- components/jobCard.tsx
+++ components/jobCard.tsx
@@ -1,16 +1,46 @@
 import { Job } from "@/types/job";
 
+const styles = {
+    cardContainer: {
+        display: "flex",
+        padding: "2rem",
+        border: "2px solid blue",
+        borderRadius: "25px",
+        backgroundColor: "lavender",
+        margin: "2rem",
+        flexDirection: "column",
+    },
+    companyTitle: {
+        textAlign: "center",
+        fontSize: "1.5rem",
+    },
+    jobInfoContainer: {
+        display: "flex",
+        flexDirection: "row",
+        margin: "0 10rem",
+        justifyContent: "space-between",
+    },
+    jobInfo: {
+        fontSize: "1.25rem",
+        borderRadius: "25px",
+        padding: "1rem",
+    },
+}
+
 type JobCardProps = {
     job: Job;
 }
 export async function JobCard({ job }: JobCardProps) {
-
+    const dateApplied= new Date(job.date_applied).toLocaleDateString();
     return (
-        <div className="jobCard">
-            <h1>{job.company_name}</h1>
-            <p>{job.job_title}</p>
-            <p>{job.job_type}</p>
-            <p>{job.date_applied.toString()}</p>
+        <div className="jobCard" style={styles.cardContainer}>
+            <h1 style={styles.companyTitle}>{job.company_name}</h1>
+            <div style={styles.jobInfoContainer}>
+                <p style={styles.jobInfo}>{job.job_title}</p>
+                <p style={styles.jobInfo}>{job.job_type}</p>
+                <p style={styles.jobInfo}>{job.location_type}</p>
+                <p style={styles.jobInfo}>{dateApplied}</p>
+            </div>
         </div>
     )
 
--- components/jobsList.tsx
+++ components/jobsList.tsx
@@ -1,13 +1,20 @@
 import { Job } from "@/types/job";
 import { JobCard} from "@/components/jobCard";
 
+const styles = {
+    listContainer: {
+        listStyleType: "none",
+        margin: "5rem 20rem",
+    }
+}
+
 
 export async function JobsList() {
     const result = await fetch("http://127.0.0.1:5000/jobs");
     const jobs: Job[] = await result.json();
 
     return (
-        <ul className="jobsList">
+        <ul className="jobsList" style={styles.listContainer}>
             {jobs.map((job) => (
                 <li key={job.id}><JobCard job={job} /></li>
             ))}
