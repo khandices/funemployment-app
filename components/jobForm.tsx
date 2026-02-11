5543a50e0de07ac8c8b99d6bd6b7a53cd6309410
khandices <s.khandice@gmail.com>
2026-02-11 09:27:20 -0500

    SPRINT-2: Update to add form and styling of form

---
+++ components/jobForm.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import {ChangeEvent, useState} from "react";
+
+
+const styles = {
+    formContainer: {
+        backgroundColor: "lavender",
+        borderRadius: "1rem",
+        boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+        display: "flex",
+        flexDirection: "column",
+        margin: "0 35rem",
+        padding: "2rem"
+    },
+    formLabel: {
+        padding: "10px",
+    },
+    formInput: {
+        padding: "10px",
+        margin: "10px"
+    },
+    formButton: {
+        margin: "10px 195px",
+        backgroundColor: "purple",
+        color: "white",
+        padding: "1rem 1.5rem",
+        borderRadius: "20px",
+        border: "0 none",
+    }
+}
+
+export function JobForm() {
+    const [job, setJob] = useState({
+        company_name: '',
+        job_title: '',
+        job_type: '',
+        date_applied: '',
+        location_type: '',
+        referral: false
+    });
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+
+        setJob( prev => ({
+            ...prev,
+              [name]: value
+        }));
+    };
+
+    const handleSubmit = async (event: SubmitEvent) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch("http://127.0.0.1:5000/jobs", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(job)
+            });
+
+            const jobData = await response.json();
+            console.log("Data submitted: ", jobData);
+            alert('Job app was submitted successfully.');
+
+        } catch (error) {
+            console.error("Error Message: ", error);
+        }
+
+    }
+
+    return (
+        <form style={styles.formContainer} onSubmit={handleSubmit}>
+            <label style={styles.formLabel}>
+                Company Name:
+                <input
+                    style={styles.formInput}
+                    name="company_name"
+                    value={job.company_name}
+                    type="text"
+                    onChange={handleChange}
+                />
+            </label>
+            <label style={styles.formLabel}>
+                Job Title:
+                <input
+                    style={styles.formInput}
+                    name="job_title"
+                    value={job.job_title}
+                    type="text"
+                    onChange={handleChange}
+                />
+            </label>
+            <label style={styles.formLabel}>
+                Job Type:
+                <input
+                    style={styles.formInput}
+                    name="job_type"
+                    value={job.job_type}
+                    type="text"
+                    onChange={handleChange}
+                />
+            </label>
+            <label style={styles.formLabel}>
+                Location Type:
+                <input
+                    style={styles.formInput}
+                    name="location_type"
+                    value={job.location_type}
+                    type="text"
+                    onChange={handleChange}
+                />
+            </label>
+            <label style={styles.formLabel}>
+                Date Applied: MM/DD/YY
+                <input
+                    style={styles.formInput}
+                    name="date_applied"
+                    value={job.date_applied}
+                    type="text"
+                    onChange={handleChange}
+                />
+            </label>
+            <label style={styles.formLabel}>
+                Referral:
+                <input
+                    style={styles.formInput}
+                    name="referral"
+                    type="checkbox"
+                    onChange={(e) =>
+                    setJob( prev => ({
+                        ...prev,
+                        referral: e.target.checked
+                    }))}
+                />
+            </label>
+            <button style={styles.formButton} type="submit" value="Add new job app">Add App</button>
+        </form>
+    )
+}
